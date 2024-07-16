834acbd2b8f44002d4760fcafda3db6e1f7a65b5
Alex Hernandez <porialex@gmail.com>
2024-07-16 21:04:21 +1000

    :bug: make instagram url parsing less strict

--- client/src/components/Embed/providers/Instagram.js
+++ client/src/components/Embed/providers/Instagram.js
@@ -1,13 +1,13 @@
 import React from "react";
 
 const extractInstagramPostId = (url) => {
-    const postIdRegex = /^https:\/\/www\.instagram\.com\/p\/([^/]+)\/?$/;
+    const postIdRegex = /^https:\/\/www\.instagram\.com\/p\/([^/]+)\//;
     const match = url.match(postIdRegex);
     return match ? match[1] : null;
 };
 
 export const isInstagramUrl = (url) => {
-    const instagramRegex = /^https:\/\/www\.instagram\.com\/p\/[^/]+\/?$/;
+    const instagramRegex = /^https:\/\/www\.instagram\.com\/p\/[^/]+\//;
     return instagramRegex.test(url);
 };
 
