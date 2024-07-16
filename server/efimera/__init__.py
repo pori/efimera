e5df4622e5c391298d84b35190b6344bed8d9ecf
Alex Hernandez <porialex@gmail.com>
2024-07-16 12:13:54 +1000

    :wrench: flask-cors

--- server/efimera/__init__.py
+++ server/efimera/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask
+from flask_cors import CORS
 
 def create_app(config_class="config.Config"):
     app = Flask(__name__)
     app.config.from_object(config_class)
+
+    CORS(app)
 
     from .extensions import db, migrate
     db.init_app(app)
