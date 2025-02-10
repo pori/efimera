85e72c7645f5ca4156f023fff5f7fdb9678c47c8
pori <porialex@gmail.com>
2025-02-10 20:21:01 +1000

    :lightning: split link creation and metadata fetch, update

--- server/efimera/tasks.py
+++ server/efimera/tasks.py
@@ -1,5 +1,5 @@
 from playwright.sync_api import sync_playwright
-from celery import shared_task
+from celery import shared_task, chain
 
 from .extensions import db
 from .models import Tag, Link, Note
@@ -8,7 +8,7 @@ import re
 
 
 @shared_task(bind=True, max_retries=3)
-def fetch_metadata(self, url):
+def fetch_metadata(self, url, link_id):
     """
     Fetch metadata from a URL using Playwright.
     Returns tuple of (title, description, image)
@@ -34,12 +34,24 @@ def fetch_metadata(self, url):
         }""")
 
         browser.close()
-
+        print(metadata.get('title'))
         return (
             metadata.get('title'),
             metadata.get('description'),
-            metadata.get('image')
+            metadata.get('image'),
+            link_id,
         )
+
+
+@shared_task(bind=True, max_retries=3)
+def save_metadata(self, title, description, image, link_id):
+    Link.query.filter_by(id=link_id).update({
+        'title': title,
+        'description': description,
+        'image': image,
+    })
+
+    db.session.commit()
 
 
 @shared_task(bind=True, max_retries=3)
@@ -50,12 +62,17 @@ def process_assets(self, note_id):
     link_matches = re.finditer(r'https?://\S+', note.text)
     for match in link_matches:
         link_url = match.group(0)
-        title, description, image = fetch_metadata.delay(link_url).get()
-        link = Link(url=link_url, title=title, description=description, image=image, note_id=note.id)
+
+        link = Link(url=link_url, note_id=note_id)
 
         db.session.add(link)
+        db.session.commit()
 
-    db.session.commit()
+        chain(
+            fetch_metadata.s(link_url, link.id),
+            save_metadata.s(),
+        )
+
 
     # Extract hashtags
     tags = re.findall(r'#\w+', note.text)
