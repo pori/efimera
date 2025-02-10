026561b18610d8c3becbb61de91e3ebc9bb22ca9
pori <porialex@gmail.com>
2025-02-10 18:01:08 +1000

    :truck: move tasks (again)

--- server/efimera/tasks.py
+++ server/efimera/tasks.py
@@ -7,6 +7,7 @@ from .models import Note, Tag, Link
 import requests
 import re
 
+
 def fetch_metadata(url):
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
@@ -27,35 +28,25 @@ def fetch_metadata(url):
 
     return title, description, image
 
-def save_data_to_db(text_blob):
-    # Save the original text to the notes table
-    note = Note(text=text_blob)
-    db.session.add(note)
-    db.session.commit()
 
-    # Extract the link (assuming it starts with "http" or "https")
-    link_match = re.search(r'https?://\S+', text_blob)
-    if link_match:
-        link_url = link_match.group(0)
+@shared_task(bind=True, max_retries=3)
+def process_assets(note):
+
+    link_matches = re.search(r'https?://\S+', note.text)
+
+    for match in link_matches:
+        link_url = match.group(0)
         title, description, image = fetch_metadata(link_url)
         link = Link(url=link_url, title=title, description=description, image=image, note_id=note.id)
 
         db.session.add(link)
-        db.session.commit()
+
+    db.session.commit()
 
     # Extract hashtags
-    tags = re.findall(r'#\w+', text_blob)
+    tags = re.findall(r'#\w+', note.text)
     for tag in tags:
         new_tag = Tag(tag=tag, note_id=note.id)
         db.session.add(new_tag)
 
     db.session.commit()
-
-
-@shared_task(bind=True, max_retries=3)
-def save_data_to_db_task(self, text_blob):
-    try:
-        save_data_to_db(text_blob)
-    except Exception as exc:
-        # Retry the task if it fails, with exponential backoff
-        raise self.retry(exc=exc, countdown=2 ** self.request.retries)
